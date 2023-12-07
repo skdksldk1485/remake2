db92ac978e16f7f3a4864f38b989584c5e726e5d
skdksldk1485 <skdksldk1485@naver.com>
2023-12-08 01:35:45 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/remake2/Remake2Application.java
@@ -0,0 +1,13 @@
+package com.example.remake2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Remake2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Remake2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/remake2/Remake2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remake2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Remake2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
